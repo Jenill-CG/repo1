ec30b615c8f4d3c758cb9d6d80cdc16d70520237
Jenill-CG <jenil@convegenius.ai>
2024-09-25 22:50:51 +0530

    Add files via upload

---
+++ gsheet.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+st.title("Read Google Sheet as DataFrame")
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read(worksheet="Example 1")
+
+st.dataframe(df)
